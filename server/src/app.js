bd048032e3f62060b3909431ab01db9e55b3605d
Myo Thu Ko <myothuko@ucsy.edu.mm>
2022-08-01 01:03:41 +0630

    finish validation in api

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const planetsRouter = require("./routes/planets/planets.router");
+const launchesRouter = require("./routes/launches/launches.router");
 
 const app = express();
 //default cors origin value *
@@ -14,8 +15,9 @@ app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "..", "public")));
 
-app.use(planetsRouter);
-app.get("/", (req, res) => {
+app.use("/planets", planetsRouter);
+app.use("/launches", launchesRouter);
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "index.html"));
 });
 
