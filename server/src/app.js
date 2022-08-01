e6b9b5d13c10f5ac50a12c7dbf0ead3e9aed060e
myothuko98 <myothuko@ucsy.edu.mm>
2022-08-01 16:01:02 +0630

    fix route for index file

--- server/src/app.js
+++ server/src/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 module.exports = app;
