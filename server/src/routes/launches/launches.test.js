e02cc2eab24caac2a5a1eaf9bc9eb6f059ce4c76
myothuko98 <myothuko@ucsy.edu.mm>
2022-08-04 09:55:55 +0630

    add pm2 and test wiht jest and suptertest

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,72 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("should return 200 OK", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: "Kepler Exploration X",
+    rocket: "Explorer IS1",
+    launchDate: "December 12, 2030",
+    target: "Kepler-442 b",
+  };
+
+  const launchDataWithoutDate = {
+    mission: "Kepler Exploration X",
+    rocket: "Explorer IS1",
+    target: "Kepler-442 b",
+  };
+
+  const launchDataWithInvalidDate = {
+    mission: "Kepler Exploration X",
+    rocket: "Explorer IS1",
+    launchDate: "zoo",
+    target: "Kepler-442 b",
+  };
+
+  test("should respond with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithoutDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required fields",
+    });
+  });
+
+  test("It should catch invalid date", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(launchDataWithInvalidDate)
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date", 
+    });
+  });
+});
