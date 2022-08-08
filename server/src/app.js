35093540b01ca8dc8047a5f7116408d4620c921b
Myo Thu Ko <myothuko@ucsy.edu.mm>
2022-08-08 14:55:00 +0630

    versioning node api

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,22 @@
-const express = require("express");
-const path = require("path");
-const cors = require("cors");
-const morgan = require("morgan");
-
-const planetsRouter = require("./routes/planets/planets.router");
-const launchesRouter = require("./routes/launches/launches.router");
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const morgan = require('morgan');
+const api = require('./routes/api');
 
 const app = express();
 //default cors origin value *
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use("/planets", planetsRouter);
-app.use("/launches", launchesRouter);
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+app.use('/v1', api);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
 });
 
 module.exports = app;
